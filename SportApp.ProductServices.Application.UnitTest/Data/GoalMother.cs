531bce7ae8339082e0f473ae8ecdc97037b4c50f
Camilo Andres Guevara Triana <c.guevarat@uniandes.edu.co>
2024-04-14 15:40:56 -0500

    add goal tests

---
+++ SportApp.ProductServices.Application.UnitTest/Data/GoalMother.cs
@@ -0,0 +1,12 @@
+﻿namespace SportApp.ProductServices.Application.UnitTest.Data ;
+using ProductsServices.Domain.Goals;
+
+    public static class GoalMother
+    {
+        public static Goal Create(string id = "7300e12e-f3df-4a8b-9847-45f07362cee9",
+            string name = "New Goal Name",
+            string user = "6ad77258-01bf-4d2b-b8ee-37e2099e0216")
+        {
+            return Goal.Build(Guid.Parse(id), name, Guid.Parse(user));
+        }
+    }
---
+++ SportApp.ProductServices.Application.UnitTest/Tests/CreateGoalUseCaseTest.cs
@@ -0,0 +1,40 @@
+﻿namespace SportApp.ProductServices.Application.UnitTest.Tests ;
+using Data;
+using Moq;
+using ProductsServices.Application.Goal.UseCases;
+using ProductsServices.Domain.Goals.Commands;
+using ProductsServices.Domain.Goals.Repositories;
+
+    public class CreateGoalUseCaseTest
+    {
+        private readonly CreateGoalUseCase _createGoalUseCase;
+        private readonly Mock<IGoalRepository> _repositoryMock;
+
+        public CreateGoalUseCaseTest()
+        {
+            _repositoryMock = new Mock<IGoalRepository>();
+            _createGoalUseCase = new CreateGoalUseCase(_repositoryMock.Object);
+        }
+
+        [Fact]
+        public async Task CreateGoalSuccessFull()
+        {
+            //Arrange
+            var goal = GoalMother.Create();
+            var request = new CreateGoalCommand
+            {
+                Id = Guid.Parse("7300e12e-f3df-4a8b-9847-45f07362cee9"),
+                Name = "New Goal",
+                User = Guid.Parse("6ad77258-01bf-4d2b-b8ee-37e2099e0216")
+            };
+            _repositoryMock.Setup(x => x.SaveAndPublishAsync(goal));
+
+            //Act
+            var response = await _createGoalUseCase.ExecuteAsync(request);
+
+            //Assert
+            Assert.Equal(request.Id, response.Id);
+            Assert.Equal(request.Name, response.Name);
+            Assert.Equal(request.User, response.CreatedBy);
+        }
+    }
