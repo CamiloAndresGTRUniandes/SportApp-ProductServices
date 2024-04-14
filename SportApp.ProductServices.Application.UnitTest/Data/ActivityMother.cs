41ea0fa27752b6b8f7ce699993476492f9d67457
Camilo Andres Guevara Triana <c.guevarat@uniandes.edu.co>
2024-04-14 15:56:31 -0500

    add activity tests

---
+++ SportApp.ProductServices.Application.UnitTest/Data/ActivityMother.cs
@@ -0,0 +1,12 @@
+﻿namespace SportApp.ProductServices.Application.UnitTest.Data ;
+using ProductsServices.Domain.Activities;
+
+    public static class ActivityMother
+    {
+        public static Activity Create(string id = "7300e12e-f3df-4a8b-9847-45f07362cee9",
+            string name = "New Activity Name",
+            string user = "6ad77258-01bf-4d2b-b8ee-37e2099e0216")
+        {
+            return Activity.Build(Guid.Parse(id), name, Guid.Parse(user));
+        }
+    }
