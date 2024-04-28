b7d3a80badceab4b4ebb6282aeeb35bce486c722
Camilo Andrés Guevara Triana <c.guevarat@uniandes.edu.co>
2024-04-28 16:40:05 -0500

    fix tracking objects

--- SportApp.ProductsServices.Infrastructure/EntityFramework/ProductService/Repositories/ProductServiceRepository.cs
+++ SportApp.ProductsServices.Infrastructure/EntityFramework/ProductService/Repositories/ProductServiceRepository.cs
@@ -109,7 +109,7 @@ using Microsoft.EntityFrameworkCore;
                     c.StartDateTime!.Value.Date <= parameters.EndDateTime!.Value.Date);
             }
 
-            productServices = await query.AsNoTracking().ToListAsync();
+            productServices = await query.ToListAsync();
             await IncludeOperations(productServices);
             return productServices;
         }
